90fea79d788d747b5c2627d12d485ca1df616e87
HendP <hendpermana12@gmail.com>
2024-05-05 13:42:08 +0700

    feat(tabs): update tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -14,7 +14,7 @@ const TabsLayout = () => {
                     tintColor={color}
                     className="w-6 h-6"
                 />
-                <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+                <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`} style={{ color: color }}>
                     {name}
                 </Text>
             </View>
@@ -26,6 +26,14 @@ const TabsLayout = () => {
             <Tabs
                 screenOptions={{
                     tabBarShowLabel: false,
+                    tabBarActiveTintColor: '#FFA001',
+                    tabBarInactiveTintColor: '#CDCDE0',
+                    tabBarStyle:{
+                        backgroundColor: '#161622',
+                        borderTopWidth: 1,
+                        borderTopColor: '#232533',
+                        height: 84,
+                    }
                 }}
             >
                 <Tabs.Screen 
